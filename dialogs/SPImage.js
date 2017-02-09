a50268a36d639a012f3cffd048ad8cf56dd02d32
Mario Duran <mario@mario-duran.com>
2017-02-08 22:48:11 -0600

    Added all the dialog functionalities and especifications

--- dialogs/SPImage.js
+++ dialogs/SPImage.js
@@ -1,37 +1,103 @@
-CKEDITOR.dialog.add('custimageDialog',
+CKEDITOR.dialog.add('SPImageDialog',
 function(editor){
   return{
-    title:'Image Properties',
+    title:'Insert Image from SharePoint',
     minWidth:400,
     minHeight:200,
     contents:[
       {
-        id:'tab-basic',
-        label:'Basic Settings',
+        id:'SPimgSettings',
+        label:'Image Settings',
         elements:[
-          {type:'text',
-          id:'src',
-          label:'Source',
-          validate:CKEDITOR.dialog.validate.notEmpty("Image source field cannot be empty")
+          {
+            type:'text',
+            id:'imgSrc',
+            label:'Image Source',
+            validate:CKEDITOR.dialog.validate.notEmpty('Image Source field cannot be empty')
         },
         {
-          type:'text',
-          id:'alt',
-          label:'Alternative'
+          type:'button',
+          id:'brsButton',
+          label:'Browse',
+          title:'Browse',
+          onClick: function() {
+              var thisDialog = CKEDITOR.dialog.getCurrent(); //get the current instance of the dialog
+
+              with(new AssetPickerConfig('spAssetPickerObj'))
+              {{
+                 DefaultAssetImageLocation='';
+                 CurrentWebBaseUrl= window._spPageContextInfo.webAbsoluteUrl; //obtain the absolute URL of the Sharepoint Site Collection
+                 OverrideDialogFeatures='';
+                 OverrideDialogTitle='';
+                 OverrideDialogDesc='';
+                 OverrideDialogImageUrl='';
+                 AssetUrlClientID=''; //not needed this time
+                 AssetTextClientID='';
+                 UseImageAssetPicker=true; //make this false to show Documents
+                 DefaultToLastUsedLocation=true;
+                 DisplayLookInSection=true;
+                 ReturnCallback = function(spi) {
+                   thisDialog.setValueOf('SPimgSettings','imgSrc',spi); //set the value of the image source field with the path of the image in SharePoint
+                 };
+               }}
+
+                APD_LaunchAssetPickerUseConfigCurrentUrl('spAssetPickerObj');
+
+             });
+          },
+          {
+            type: 'text',
+            id: 'imgHeight',
+            label: 'Height',
+            maxLength: 5,
+            size: 50,
+            validate: function() {
+              if (this.getValue()) {
+                if(isNaN(parseFloat(this.getValue()))) {
+                  api.openMsgDialog( '', 'Height must be numeric.' );
+                  return false;
+                }
+              }
+            }
+          },
+          {
+            type: 'text',
+            id: 'imgWidth',
+            label: 'Width',
+            maxLength: 5,
+            size: 50,
+            validate: function() {
+              if (this.getValue()) {
+                if(isNaN(parseFloat(this.getValue()))) {
+                  api.openMsgDialog( '', 'Width must be numeric.' );
+                  return false;
+                }
+              }
+            }
+          }
         }
       ]
     }
   ],
   onShow:function(){
-    CKEDITOR.dialog.getCurrent().hide();
-    var my=window.open("ckeditor/plugins/custimage/dialogs/dialogtest.html","ImageBrowser","menubar=1,resizable=1,width=950,height=580");//change this code to your desired page
-    my.focus()
+    //UGLY hack:
+    //set Z-INDEX to be just behind the SharePoint modals Z-Index
+    $('div.cke_dialog_background_cover').css('z-index', 1501);
+
+    $('table.cke_dialog.cke_ltr.cke_single_page').css('z-index', 1502);
   },
   onOk:function(){
-    var dialog=this;
-    var custimage=editor.document.createElement('img');
-    custimage.setAttribute('src',dialog.getValueOf('tab-basic','src'));
-    custimage.setAttribute('alt',dialog.getValueOf('tab-basic','alt'));
-    editor.insertElement(custimage)
+    var dialog = this;
+    var imageObj = editor.document.createElement('img');
+    imageObj.setAttribute('src', dialog.getValueOf('SPimgSettings', 'imgSrc'));
+    if(dialog.getValueOf('SPimgSettings', 'imgHeight') != '') {
+      imageObj.setStyle('height', dialog.getValueOf('SPimgSettings', 'imgHeight'));
+    }
+
+    if(dialog.getValueOf('SPimgSettings', 'imgWidth') != '') {
+      imageObj.setStyle('width', dialog.getValueOf('SPimgSettings', 'imgWidth'));
+    }
+
+    editor.insertElement(imageObj); //insert the image
   }
 }});
